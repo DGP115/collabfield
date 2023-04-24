2e75b7b8b98e152b336af1e96e0b8a44ecaaf909
DGP115 <david.g.peloso@gmail.com>
2023-04-24 13:55:24 -0400

    Login setup

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -4,9 +4,9 @@
 module NavigationHelper
   def collapsible_links_partial_path
     if user_signed_in?
-      'layouts/navigation/collapsible_elements/signed_link_links'
+      'layouts/navigation/collapsible_elements/signed_in_links'
     else
-      'layouts/navigation/collapsible_elements/not_signed_link_links'
+      'layouts/navigation/collapsible_elements/not_signed_in_links'
     end
   end
 end
